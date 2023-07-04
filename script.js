97cda23da9cc24ba0c9faec49f336e0a57aa25d1
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-07-04 17:42:31 -0400

    clean up code - getElementById

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const game = () => {
   const gamePieces = Math.floor(Math.random() * 3);
   switch (gamePieces) {
     case 0:
-      document.getElementById(
-        "#result"
-      ).innerHTML = `<i class="fa-regular fa-hand-back-fist"></i>`;
+      document.getElementById("#result");
+      result.innerHTML = `<i class="fa-regular fa-hand-back-fist"></i>`;
       break;
     case 1:
-      document.getElementById(
-        "#result"
-      ).innerHTML = `<i class="fa-regular fa-hand"></i>`;
+      document.getElementById("#result");
+      result.innerHTML = `<i class="fa-regular fa-hand"></i>`;
       break;
     case 2:
-      document.getElementById(
-        "#result"
-      ).innerHTML = `<i class="fa-regular fa-hand-scissors"></i>`;
+      document.getElementById("#result");
+      result.innerHTML = `<i class="fa-regular fa-hand-scissors"></i>`;
       break;
   }
 };
+
+const btn = document.querySelector("#goBtn");
+btn.addEventListener("click", game);
